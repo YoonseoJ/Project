5b32f7eaa6e66ad3e24ce10abd7733364ea68032
YoonseoJ <yoonseo.jeong@student.vcc.ca>
2022-02-07 00:27:44 -0800

    design update

--- src/components/hompage.jsx
+++ src/components/hompage.jsx
@@ -8,6 +8,18 @@ export default function Homepage() {
                 <div className="homepage_background2">
                     
                 </div>
+                <div className="background_img_div">
+                    <img src="cupcake.png" alt="" className="background_img"/>
+                </div>
+                <div className="background_img_div2">
+                    <img src="chococupcake.png" alt="" className="background_img2"/>
+                </div>
+                <div className="background_img_div3">
+                    <img src="berrycake.png" alt="" className="background_img3"/>
+                </div>
+                <div className="background_img_div4">
+                    <img src="croffle.png" alt="" className="background_img4"/>
+                </div>
                 <div className="homepage_text_div">
                     <p>Taste Some Joy</p>
                     <p>And Let's Be </p>
